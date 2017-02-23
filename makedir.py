8fb011706622c61fa964cec0a087b2550f438b16
OracleAndroid <dbowers88@gmail.com>
2017-02-23 04:30:34 -0700

    Added path input, re-tooled Directory creation. Setup for file ID and move

--- makedir.py
+++ makedir.py
@@ -1,28 +1,45 @@
+from PIL import Image
+import glob
 import os
 
+#get and print current path
+cpath = os.getcwd()
+print ("Current directory:", cpath);
+
+#get path from user
+print ("Please input the path to folder:")
+pathin = input();
+os.chdir(pathin)
+
+#Print current path
+npath = os.getcwd()
+inpath = os.listdir()
+print ("Current directory:", npath)
+print ("Files in directory:", inpath);
+input ('Press any key to create folders for images.');
+
 #Create Directories for sorting
-newpath = r'/home/dave/Workspace/Projects/WallpaperSort/1080P'
+newpath = r'1080P'
 if os.path.exists(newpath):
     print ("1080P Folder already exists")
 if not os.path.exists(newpath):
         os.makedirs(newpath)
-newpath = r'/home/dave/Workspace/Projects/WallpaperSort/OtherSize'
+newpath = r'OtherSize'
 if os.path.exists(newpath):
     print ("OtherSize Folder already exists")
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-newpath = r'/home/dave/Workspace/Projects/WallpaperSort/Ultrawide'
+newpath = r'Ultrawide'
 if os.path.exists(newpath):
     print ("Ultrawide Folder already exists")
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-newpath = r'/home/dave/Workspace/Projects/WallpaperSort/FourByThree'
+newpath = r'FourByThree'
 if os.path.exists(newpath):
     print ("FourByThree Folder already exists")
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-newpath = r'/home/dave/Workspace/Projects/WallpaperSort/Phone'
+newpath = r'Phone'
 if os.path.exists(newpath):
-    print ("Phone Folder already exists")
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
--- sort.py
+++ sort.py
@@ -1,9 +1,16 @@
 from PIL import Image
-import glob, os
+import glob
+import os
+import shutil
 
 input ('Press any key to sort.')
 
+#Get image dimensions
 for image_file in glob.glob("*.*g"):
     file, ext = os.path.splitext(image_file)
-    im = Image.open(image_file)
-    print ("", im.size)
+    im = Image.load(image_file)
+    im.size = width, height
+    print("", im.size)
+    src = os.getcwd
+    if width("1920"):
+        shutil.move(src,'1080P')
