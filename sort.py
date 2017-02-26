e3387083f3452a1fa80852f1ec69e68f7dd45c40
OracleAndroid <dbowers88@gmail.com>
2017-02-26 00:10:00 -0700

    Add files via upload

--- sort.py
+++ sort.py
@@ -8,9 +8,11 @@ input ('Press any key to sort.')
 #Get image dimensions
 for image_file in glob.glob("*.*g"):
     file, ext = os.path.splitext(image_file)
-    im = Image.load(image_file)
-    im.size = width, height
-    print("", im.size)
+    im = Image.open(image_file)
+    print ("", im.width, im.height)
+    width = im.width
+    height = im.height
     src = os.getcwd
-    if width("1920"):
+#Match Width and Height, then move to corresponding folder.
+    if width('1920'):
         shutil.move(src,'1080P')
