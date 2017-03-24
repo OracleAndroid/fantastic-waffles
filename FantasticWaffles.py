78918385ba60280b5a745d1655246dcffcd77e3a
OracleAndroid <dbowers88@gmail.com>
2017-03-24 09:26:40 -0700

    Combined parts. BOILERPLATE MY SHIT UP FAM

--- FantasticWaffles.py
+++ FantasticWaffles.py
@@ -11,18 +11,19 @@ print ("Current directory:", cpath);
 #get path from user
 print ("Please input the path to folder:")
 pathin = input();
+
 if os.path.exists(pathin):
     os.chdir(pathin)
     print("Changed to path:", pathin)
-if not os.path.exists(pathin):
+elif not os.path.exists(pathin):
     print("Directory not found! Create Directory? Yes(1) or No(2):")
 nopath = input()
 usrin = "1"
 if nopath == usrin:
     os.mkdir(pathin)
     os.chdir(pathin)
-else:
-    quit()
+#else:
+#    quit()
 
 #Print current path
 npath = os.getcwd()
@@ -52,11 +53,6 @@ if os.path.exists(newpath):
     print ("4x3 Folder already exists")
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-newpath = r"Phone"
-if os.path.exists(newpath):
-    print("Phone Folder already exists")
-if not os.path.exists(newpath):
-        os.makedirs(newpath)
 newpath = r"4K"
 if os.path.exists(newpath):
     print("4K Folder already exists")
@@ -67,3 +63,44 @@ if os.path.exists(newpath):
     print("16x10 Folder already exists")
 if not os.path.exists(newpath):
     os.makedirs(newpath)
+
+input ("Press any key to sort.")
+#Get image dimensions
+for image_file in glob.glob("*.*g"):
+    file, ext = os.path.splitext(image_file)
+    im = Image.open(image_file)
+    print ("", im.size)
+    if im.size == (1920, 1080): #Match Width and Height, then move to corresponding folder.
+        shutil.move(image_file, "16x9")
+    elif im.size == (2560, 1440):
+        shutil.move(image_file, "16x9")
+    elif im.size == (1360, 768):
+        shutil.move(image_file, "16x9")
+    elif im.size == (1366, 768):
+        shutil.move(image_file, "16x9")
+    elif im.size == (1600, 900):
+        shutil.move(image_file, "16x9")
+    elif im.size == (2560, 1080):
+        shutil.move(image_file, "Ultrawide")
+    elif im.size == (3440, 1440):
+        shutil.move(image_file, "Ultrawide")
+    elif im.size == (1900, 1200):
+        shutil.move(image_file, "16x10")
+    elif im.size == (2560, 1600):
+        shutil.move(image_file, "16x10")
+    elif im.size == (1920, 1200):
+        shutil.move(image_file, "16x10")
+    elif im.size == (1680, 1050):
+        shutil.move(image_file, "16x10")
+    elif im.size == (1280, 960):
+        shutil.move(image_file, "4x3")
+    elif im.size == (1024, 768):
+        shutil.move(image_file, "4x3")
+    elif im.size == (1280, 1024):
+        shutil.move(image_file, "4x3")
+    elif im.size == (800, 600):
+        shutil.move(image_file, "4x3")
+    elif im.size == (3840, 2160):
+        shutil.move(image_file, "4K")
+    else:
+        shutil.move(image_file, "OtherSizes")
